e59b13c0462ee8cf9dc299128f40014b47ca0eae
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-05-06 12:40:11 +0200

    feat: add sockets to epoll interest list

--- src/mishell.s
+++ src/mishell.s
@@ -6,6 +6,10 @@ epoll_fd  resw 1
 tcp_fd    resw 1
 unix_fd   resw 1
 
+epoll_event_t:
+  .events resd 1
+  .data   resw 1
+
 section .data
 
 section .text
@@ -15,6 +19,8 @@ _start:
   mov   rax, 291  ; EPOLL_CREATE1
   xor   di, di
   syscall
+  cmp   ax, 0
+  jl    .error
 
   mov   [epoll_fd], ax
   
@@ -24,10 +30,23 @@ _start:
   mov   si, 2   ; SOCK_STREAM
   xor   dx, dx 
   syscall
+  cmp   ax, 0
+  jl    .error
 
   mov   [tcp_fd], ax
 
-  ; add tcp socket to the epoll instance
+  ; add tcp socket to the epoll interest list
+  mov   dword [epoll_event_t.events], 1 ; EPOLLIN
+  mov   word [epoll_event_t.data], ax
+
+  mov   rax, 233  ; EPOLL_CTL
+  mov   di, [epoll_fd]
+  mov   si, 1     ; EPOLL_CTL_ADDA
+  mov   dx, [tcp_fd]
+  mov   r10, epoll_event_t
+  syscall
+  cmp   ax, 0
+  jl    .error
 
   ; create unix socket
   mov   rax, 41 ; SOCKET
@@ -35,10 +54,23 @@ _start:
   mov   si, 2   ; SOCK_STREAM
   xor   dx, dx 
   syscall
+  cmp   ax, 0
+  jl    .error
 
   mov   [unix_fd], ax
 
-  ; add unix socket to the epoll instance
+  ; add unix socket to the epoll interest list
+  mov   dword [epoll_event_t.events], 1 ; EPOLLIN
+  mov   word [epoll_event_t.data], ax
+
+  mov   rax, 233  ; EPOLL_CTL
+  mov   di, [epoll_fd]
+  mov   si, 1     ; EPOLL_CTL_ADDA
+  mov   dx, [unix_fd]
+  mov   r10, epoll_event_t
+  syscall
+  cmp   ax, 0
+  jl    .error
 
   ; bind tcp socket
 
@@ -51,17 +83,17 @@ _start:
 
   ; close tcp socket
   mov   rax, 3  ; CLOSE
-  mov   di, word [tcp_fd]
+  mov   di, [tcp_fd]
   syscall
   
   ; close unix socket
   mov   rax, 3  ; CLOSE
-  mov   di, word [unix_fd]
+  mov   di, [unix_fd]
   syscall
 
   ; close epoll socket
   mov   rax, 3  ; CLOSE
-  mov   di, word [epoll_fd]
+  mov   di, [epoll_fd]
   syscall
 
   mov   rdi, 0
