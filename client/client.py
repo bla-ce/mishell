e7513579bb7998f215cb56e88e1d184b12998a9f
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-05-13 17:30:47 +0200

    dont get IP from unix socket

--- client/client.py
+++ client/client.py
@@ -9,13 +9,16 @@ from packet import (
 
 HOST_MAX_LEN = 8
 
+SOCKET_PATH = 'mishell.sock'
+MISHELL_PORT = 7474
+
 def recv_packet(sock: socket.socket) -> Packet:
     return Packet.unpack(sock.recv(4096))
 
 # -- TCP tests --
 
 print("TEST (tcp): sending invalid magic value should fail")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(magic=WRONG_MAGIC, op=OP_HELLO,
                         flags=FL_CLIENT_TO_SERVER | FL_HOST).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -26,7 +29,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     assert resp.payload == b'invalid magic value in packet', f"bad payload: {resp}"
 
 print("TEST (tcp): sending wrong direction should fail")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_HELLO,
                         flags=FL_SERVER_TO_CLIENT | FL_HOST).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -36,7 +39,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     assert resp.payload == b'invalid direction flag in packet', f"bad payload: {resp}"
 
 print("TEST (tcp): sending wrong mode should fail")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_HELLO,
                         flags=FL_CLIENT_TO_SERVER | FL_SERVER).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -46,7 +49,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     assert resp.payload == b'invalid mode flag in packet', f"bad payload: {resp}"
 
 print("TEST (tcp): sending wrong op should fail")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=0x4,
                         flags=FL_CLIENT_TO_SERVER | FL_USER).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -56,7 +59,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     assert resp.payload == b'invalid op value in packet', f"bad payload: {resp}"
 
 print("TEST (tcp): sending HELLO should work and return a WELCOME op")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_HELLO,
                         flags=FL_CLIENT_TO_SERVER | FL_USER).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -68,8 +71,9 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
 
 host_id = 0
 
-print("TEST (tcp): sending AUTH should work and return a AUTH_OK op with 16 bytes payload")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+print("TEST (unix): sending AUTH should work and return a AUTH_OK op with 16 bytes payload")
+with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+    sock.connect(SOCKET_PATH)
     sock.sendall(Packet(op=OP_AUTH,
                         flags=FL_CLIENT_TO_SERVER | FL_USER).pack())
     sock.shutdown(socket.SHUT_WR)
@@ -83,7 +87,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     host_id = int.from_bytes(resp.payload, byteorder='little')
 
 print("TEST (tcp): sending AUTH with id should return empty payload")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_AUTH,
                         flags=FL_CLIENT_TO_SERVER | FL_USER,
                         id=host_id).pack())
@@ -96,7 +100,7 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
     assert len(resp.payload) == 0, f"payload should be empty: {resp}"
 
 print("TEST (tcp): sending AUTH with wrong id should return ERROR: host not found")
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_AUTH,
                         flags=FL_CLIENT_TO_SERVER | FL_USER,
                         id=0x12345).pack())
@@ -110,14 +114,14 @@ with socket.create_connection(('127.0.0.1', 7474)) as sock:
 
 print("TEST (tcp): sending AUTH should return a message after registering too many hosts")
 for _ in range(HOST_MAX_LEN-1): # we already registered one
-    with socket.create_connection(('127.0.0.1', 7474)) as sock:
+    with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
         sock.sendall(Packet(op=OP_AUTH,
                             flags=FL_CLIENT_TO_SERVER | FL_USER).pack())
         sock.shutdown(socket.SHUT_WR)
         resp = recv_packet(sock)
         assert resp.op == OP_AUTH_OK, f"expected AUTH_OK: {resp}"
 
-with socket.create_connection(('127.0.0.1', 7474)) as sock:
+with socket.create_connection(('127.0.0.1', MISHELL_PORT)) as sock:
     sock.sendall(Packet(op=OP_AUTH,
                         flags=FL_CLIENT_TO_SERVER | FL_USER).pack())
     sock.shutdown(socket.SHUT_WR)
