e259979ec7cefff5a71a0ecfd48f89a34b4f3814
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-05-15 15:12:46 +0200

    auth test

--- tests/unit/src/auth_test.s
+++ tests/unit/src/auth_test.s
@@ -5,6 +5,22 @@ global _start
 
 section .text
 _start:
-  mov   rax, SYS_EXIT
+  ; rand_generate_number should generate a number without error
+  call  rand_generate_number
+  cmp   rdx, SUCCESS_CODE
+  jne   .error
+
+  ; generate_id should generate a 16 bytes id without error
+  call  generate_id
+  cmp   rax, SUCCESS_CODE
+  jne   .error
+
   mov   rdi, SUCCESS_CODE
+  jmp   .exit
+
+.error:
+  mov   rax, FAILURE_CODE
+
+.exit:
+  mov   rax, SYS_EXIT
   syscall
