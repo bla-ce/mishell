6e059b660d09aae0cb83f25c8b408f5917c809cb
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-06-04 20:19:51 +0200

    feat: unit tests for host_get_by_id

--- tests/unit/src/host/host_test.s
+++ tests/unit/src/host/host_test.s
@@ -18,9 +18,69 @@ usage_str     db "usage: mishell init", LINE_FEED
               db "       mishell connect <ip> <port>", LINE_FEED
 usage_str_len equ $ - usage_str
 
+dummy_id_1 dq 0x1234
+           dq 0x5678
+
+dummy_id_2 dq 0x2345
+           dq 0x6789
+
+dummy_id_3 dq 0x9876
+           dq 0x5432
+
+section .data
+
+host_ptr dq 0
+
 section .text
 _start:
+  ; --- host_get_by_id ---
+  ; empty array should return -1
+  mov   rdi, dummy_id_1
+  call  host_get_by_id
+  cmp   rax, FAILURE_CODE
+  jne   .error
+
+  lea   rdi, [hosts]
+  mov   rax, qword [dummy_id_1]
+  mov   rdx, qword [dummy_id_1+0x8]
+  mov   [rdi+HOST_T_OFF_ID], rax
+  mov   [rdi+HOST_T_OFF_ID+0x8], rdx
+  mov   [host_ptr], rdi
+
+  lea   rdi, [hosts]
+  add   rdi, HOST_T_LEN
+  mov   rax, qword [dummy_id_2]
+  mov   rdx, qword [dummy_id_2+0x8]
+  mov   [rdi+HOST_T_OFF_ID], rax
+  mov   [rdi+HOST_T_OFF_ID+0x8], rdx
+
+  mov   byte [curr_host_idx], 2
+
+  ; filled array without the right id should return -1
+  mov   rdi, dummy_id_3
+  call  host_get_by_id
+  cmp   rax, FAILURE_CODE
+  jne   .error
+
+  ; null pointer argument should return -1
+  xor   rdi, rdi
+  call  host_get_by_id
+  cmp   rax, FAILURE_CODE
+  jne   .error
+
+  ; valid id should return the pointer to the host
+  mov   rdi, dummy_id_1
+  call  host_get_by_id
+  cmp   rax, [host_ptr]
+  jne   .error
+
   ; --- host_addr_exists ---
+  ; reset hosts array
+  mov   rdi, hosts
+  xor   rax, rax
+  mov   rcx, HOSTS_LEN
+  rep   stosb
+
   ; test without hosts
   mov   rdi, 0
   mov   rsi, 7474
