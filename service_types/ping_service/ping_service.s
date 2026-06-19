7d8df5a9ae1a41ef0b9c70bd97928ff85d317cb1
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-06-19 18:40:16 +0200

    service_type validation can be done at build time, so cool

--- service_types/ping_service/ping_service.s
+++ service_types/ping_service/ping_service.s
@@ -4,7 +4,7 @@ ping_service_t:
   .name         db "PING", NULL_CHAR
   .pad          times SERVICE_TYPE_NAME_MAX_LEN - ($ - ping_service_t.name) db 0
   .commands     dq ping_command_ping
-  .pad2         times COMMANDS_MAX_COUNT_PER_SERVICE_TYPE - 1 dq 0
+  .pad2         times (COMMANDS_MAX_COUNT_PER_SERVICE_TYPE * 8) - ($ - ping_service_t.commands) dq 0
   .description  db "A simple service returning pong when receiving ping", NULL_CHAR
   .pad3         times SERVICE_TYPE_DESC_MAX_LEN - ($ - ping_service_t.description) db 0
 ping_service_t_end:
