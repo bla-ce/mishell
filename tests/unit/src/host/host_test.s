fcbeea14f052b779fd1e7ecb344d6263005d0db4
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-06-20 11:14:53 +0200

    test: unit host_init

--- tests/unit/src/host/host_test.s
+++ tests/unit/src/host/host_test.s
@@ -11,16 +11,21 @@ global _start
 
 section .rodata
 
-dummy_host_1 dq "home", NULL_CHAR
+dummy_host_1 db "home", NULL_CHAR
 dummy_host_1_len equ $ - dummy_host_1
 
-dummy_host_2 dq "lab", NULL_CHAR
+dummy_host_2 db "lab", NULL_CHAR
 dummy_host_2_len equ $ - dummy_host_2
 
-dummy_host_3 dq "default", NULL_CHAR
+dummy_host_3 db "default", NULL_CHAR
 dummy_host_3_len equ $ - dummy_host_3
 
-section .data
+invalid_host_name times HOST_NAME_MAX_LEN db 'A'
+                  db NULL_CHAR
+
+section .bss
+
+test_host resb HOST_T_LEN
 
 section .text
 _start:
@@ -122,6 +127,77 @@ _start:
   mov   rsi, 0
   call  host_addr_exists
   cmp   rax, FAILURE_CODE
+  jne   .error
+
+  ; host init
+  ; reset hosts
+  mov   rdi, hosts
+  xor   al, al
+  mov   rcx, HOSTS_LEN
+  rep   stosb
+
+  ; empty host pointer should return failure
+  xor   rdi, rdi
+  xor   rsi, rsi
+  mov   rdx, 7474
+  mov   rcx, dummy_host_1
+  call  host_init
+  cmp   rax, error_codes.INTERNAL
+  jne   .error
+
+  ; empty host name should return failure
+  mov   rdi, test_host
+  xor   rsi, rsi
+  mov   rdx, 7474
+  xor   rcx, rcx
+  call  host_init
+  cmp   rax, error_codes.INTERNAL
+  jne   .error
+
+  ; happy path
+  mov   rdi, test_host
+  xor   rsi, rsi
+  mov   rdx, 7474
+  mov   rcx, dummy_host_1
+  call  host_init
+  cmp   rax, SUCCESS_CODE
+  jne   .error
+
+  cmp   word [test_host+HOST_T_OFF_PORT], 7474
+  jne   .error
+  cmp   dword [test_host+HOST_T_OFF_IP], 0
+  jne   .error
+
+  lea   rdi, [test_host+HOST_T_OFF_NAME]
+  mov   rsi, dummy_host_1
+  call  strcmp
+  cmp   rax, TRUE
+  jne   .error
+
+  ; add it to hosts
+  mov   rdi, hosts
+  mov   rsi, test_host
+  mov   rcx, HOST_T_LEN
+  rep   movsb
+
+  mov   byte [curr_host_idx], 1
+
+  ; duplicated addr should fail
+  mov   rdi, test_host
+  xor   rsi, rsi
+  mov   rdx, 7474
+  mov   rcx, dummy_host_2
+  call  host_init
+  cmp   rax, error_codes.HOST_ADDR_ALREADY_EXISTS
+  jne   .error
+
+  ; invalid host name should return INTERNAL
+  mov   rdi, test_host
+  xor   rsi, rsi
+  mov   rdx, 5959
+  mov   rcx, invalid_host_name
+  call  host_init
+  cmp   rax, error_codes.INTERNAL
   jne   .error
 
   mov   rdi, SUCCESS_CODE
