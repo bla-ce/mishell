4888fc4614e9ece3bcbf886ea17e0b1e2e714a03
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-06-20 14:30:47 +0200

    cli: connect to remote host

--- cli/mishli.s
+++ cli/mishli.s
@@ -9,11 +9,66 @@
 
 global _start
 
+section .rodata
+
+mishli_usage_str db ""
+  db "usage:  mishli --host <host_addr> HELLO                                   -> check if host is up", LINE_FEED
+  db "        mishli --host <host_addr> REGISTER <host_id> <type>               -> register new service", LINE_FEED
+  db "        mishli --host <host_addr> START <host_id> <service_id>            -> start service", LINE_FEED
+  db "        mishli --host <host_addr> STOP <host_id> <service_id>             -> stop service", LINE_FEED
+  db "        mishli --host <host_addr> UNREGISTER <host_id> <service_id>       -> unregister service", LINE_FEED
+  db "        mishli --host <host_addr> QUERY <host_id> <service_id> <command>  -> query a running service", LINE_FEED
+  db "        mishli --host <host_addr> CATALOG                                 -> list available service types", LINE_FEED
+  db "        mishli --host <host_addr> NETWORK                                 -> list available hosts", NULL_CHAR
+
+MISHLI_MIN_ARG equ 0x4
+
+HOST_FLAG db "--host", NULL_CHAR
+
+section .data
+
+host_fd dq 0
+
 section .text
 
 _start:
+  ; STACK USAGE
+  ; [rsp]       -> argc
+  ; [rsp+0x8]   -> pointer to program name
+  ; [rsp+0x10]  -> pointer to host flag
+  ; [rsp+0x18]  -> pointer to host address
+  ; [rsp+0x20]  -> pointer to op
+  ; [rsp+0x28]  -> first argument (host id)
+  ; [rsp+0x30]  -> first argument (service id)
+  ; [rsp+0x38]  -> first argument (service type or command)
+
+  cmp   qword [rsp], MISHLI_MIN_ARG
+  jl    .usage
+
+  ; make sure host flag is set
+  mov   rdi, [rsp+0x10]
+  mov   rsi, HOST_FLAG
+  call  strcmp
+  cmp   rax, TRUE
+  jne   .usage
+
+  ; connect to remote host
+  mov   rdi, [rsp+0x18]
+  call  mishli_connect_to_host
+  cmp   rax, 0
+  jl    .usage
+
+  ; close fd
+  mov   rax, SYS_CLOSE
+  mov   rdi, [host_fd]
+  syscall
+
   mov   rdi, SUCCESS_CODE
   jmp   .exit
+
+.usage:
+  mov   rdi, mishli_usage_str
+  call  println
 
 .error:
   mov   rax, SYS_EXIT
@@ -23,3 +78,74 @@ _start:
 .exit:
   mov   rax, SYS_EXIT
   syscall
+
+; connects to the host before sending the request
+; @param  rdi: pointer to the host address
+; @return rax: return code
+mishli_connect_to_host:
+  sub   rsp, 0x18
+
+  ; STACK USAGE
+  ; [rsp]       -> pointer to the host address
+  ; [rsp+0x8]   -> ip of the remote host
+  ; [rsp+0x10]  -> port of the remote host
+
+  mov   [rsp], rdi
+
+  test  rdi, rdi
+  jz    .error
+
+  ; parse host address
+.loop:
+  ; find colon
+  cmp   byte [rdi], COLON
+  jne   .continue
+
+  mov   rax, NULL_CHAR
+  stosb
+
+  jmp   .loop_end
+
+.continue:
+  cmp   byte [rdi], NULL_CHAR
+  je    .error    ; wrong format
+
+  inc   rdi
+
+  jmp   .loop
+.loop_end:
+  ; rdi points to the port
+  call  atoi
+  cmp   rax, 0
+  jl    .error
+
+  mov   [rsp+0x10], rax
+
+  mov   rdi, [rsp]
+  call  inet_pton
+  cmp   rax, 0
+  jl    .error
+
+  mov   [rsp+0x8], rax
+
+  mov   rdi, [rsp+0x8]
+  mov   rsi, [rsp+0x10]
+  call  net_tcp_connect_to_remote_addr
+  cmp   rax, 0
+  jl    .error
+
+  mov   [host_fd], rax
+
+  mov   rax, SUCCESS_CODE
+  jmp   .return
+
+.error:
+  mov   rax, SYS_CLOSE
+  mov   rdi, [host_fd]
+  syscall
+
+  mov   rax, FAILURE_CODE
+
+.return:
+  add   rsp, 0x18
+  ret
